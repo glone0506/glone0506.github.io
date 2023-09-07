4898d9fde3b344b37a0ccfd430aa2fb577bbb54c
Glone <glone0506@gmail.com>
2023-09-07 17:34:35 +0900

    Add files via upload

---
+++ static/js/675.940ee2a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadminpro=self.webpackChunkadminpro||[]).push([[675],{9088:function(n,e,r){r.r(e);var i=r(3009),s=r(3072),u=r(184);e.default=function(){return(0,u.jsx)("div",{children:(0,u.jsx)(i.Z,{children:(0,u.jsx)(s.Z,{})})})}}}]);
+//# sourceMappingURL=675.940ee2a4.chunk.js.map
